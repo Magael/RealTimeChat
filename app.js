cbae6d241d006892c7723e91c245232335851063
ricaneu <79845716+ricaneu@users.noreply.github.com>
2021-09-29 22:56:09 +0200

    move  chat to project

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require('express');
 const path = require('path');
 const pageRouter = require('./routes/pages');
 const app = express();
+const http = require('http');
+const server = http.createServer(express);
+const { Server } = require("socket.io");
+const io = new Server(server);
 
 // used by body-parser
 app.use(express.urlencoded({extended : false}));
@@ -15,6 +19,13 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+//this code to router
+app.get('/chat', (req, res) => {
+    res.sendFile(__dirname + '/chat.html');
+  });
+
+
+//this is disable. code from above should be aded to router
 //routers
 app.use('/', pageRouter);
 
@@ -31,6 +42,13 @@ app.use((err,req, res, next)=>{
     res.send(err.message);
 })
 
+io.on('connection', (socket) => {
+    socket.on('chat message', (msg) => {
+      socket.broadcast.emit(msg);
+      io.emit('chat message', msg);
+      console.log('message: ' + msg.text);
+    });
+  });
 
 //setting server
 app.listen(5050, ()=>{
