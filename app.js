cc268103d69245c0bc39699b91653a9e3e8a8fe3
alain17-web <web2020.alain@gmail.com>
2021-09-30 11:55:33 +0200

    .ejs

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 //templates
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set('view engine', 'ejs');
 
 //routers
 app.use('/', pageRouter);
